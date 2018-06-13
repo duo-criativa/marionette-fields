ad70aa50bb0455e08b8d20e8639b6502c3d75abd
Paulo Ribeiro <ribeiro.paulor@gmail.com>
2018-06-13 12:23:02 -0300

    - exported `InputState`.

--- src/InputView.js
+++ src/InputView.js
@@ -1,104 +1,9 @@
 var pick = require('lodash/pick');
 var Mn = require('backbone.marionette');
-var State = require('ampersand-state');
+var InputState = require('./InputState');
 require('marionette-bindings');
 
-var InputState = State.extend({
-  session: {
-    tests: 'any'
-  },
 
-  props: {
-    inputValue: 'any',
-    startingValue: 'any',
-    name: 'string',
-    prefix_html_id: ['string', true, 'form_field_'],
-    type: ['string', true, 'text'],
-    placeholder: ['string', true, ''],
-    label: ['string', true, ''],
-    required: ['boolean', true, true],
-    helpMessage: ['string', true, ''],
-    directlyEdited: ['boolean', true, false],
-    readonly: ['boolean', true, false],
-    autofocus: ['boolean', true, false],
-    shouldValidate: ['boolean', true, false],
-    message: ['string', true, ''],
-    requiredMessage: ['string', true, 'This field is required.'],
-    validClass: ['string', true, 'input-valid'],
-    invalidClass: ['string', true, 'input-invalid'],
-    validityClassSelector: ['string', true, 'input, textarea'],
-    tabindex: ['number', true, 0],
-  },
-
-  derived: {
-    html_id: {
-      deps: ['name', 'prefix_html_id'],
-      fn: function() {
-        return this.prefix_html_id + this.name;
-      }
-    },
-    value: {
-      deps: ['inputValue'],
-      fn: function () {
-        return this.inputValue;
-      }
-    },
-    valid: {
-      cache: false,
-      deps: ['inputValue'],
-      fn: function () {
-        return !this.runTests();
-      }
-    },
-    showMessage: {
-      deps: ['message', 'shouldValidate'],
-      fn: function () {
-        return this.shouldValidate && this.message;
-      }
-    },
-    changed: {
-      deps: ['inputValue', 'startingValue'],
-      fn: function () {
-        return this.inputValue !== this.startingValue;
-      }
-    },
-    validityClass: {
-      deps: ['valid', 'validClass', 'invalidClass', 'shouldValidate'],
-      fn: function () {
-        if (!this.shouldValidate) {
-          return '';
-        } else {
-          return this.valid ? this.validClass : this.invalidClass;
-        }
-      }
-    }
-  },
-
-  getErrorMessage: function () {
-    var message = '';
-    if (this.required && this.value === '') {
-      return this.requiredMessage;
-    } else {
-      (this.tests || []).some(function (test) {
-        message = test.call(this, this.value) || '';
-        return message;
-      }, this);
-      return message;
-    }
-  },
-  runTests: function () {
-    var errorMessage = this.getErrorMessage();
-    if (!errorMessage && this.inputValue && this.changed) {
-      // if it's ever been valid,
-      // we want to validate from now
-      // on.
-      this.shouldValidate = true;
-    }
-    this.message = errorMessage;
-    return errorMessage;
-  },
-
-});
 
 var InputView = Mn.View.extend({
   tagName: 'div',
@@ -342,5 +247,4 @@ var InputView = Mn.View.extend({
   }
 });
 
-module.exports = InputState;
 module.exports = InputView;
